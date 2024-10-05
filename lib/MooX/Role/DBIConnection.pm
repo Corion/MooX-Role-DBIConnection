ac3d48342e5cd5540c0600779f4b6ff47ba0f9af
Max Maischein <corion@corion.net>
2024-10-05 10:36:42 +0200

    Bump copyright notice

--- lib/MooX/Role/DBIConnection.pm
+++ lib/MooX/Role/DBIConnection.pm
@@ -120,7 +120,7 @@ Max Maischein C<corion@cpan.org>
 
 =head1 COPYRIGHT (c)
 
-Copyright 2019-2023 by Max Maischein C<corion@cpan.org>.
+Copyright 2019-2024 by Max Maischein C<corion@cpan.org>.
 
 =head1 LICENSE
 
